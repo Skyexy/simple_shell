bab3b637d10563502eef85ecfe08976bad1795f4
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-05-06 17:34:29 +0100

    Update simple_shell.c

--- simple_shell.c
+++ simple_shell.c
@@ -5,7 +5,7 @@ int main(int ac __attribute__((unused)), char **av __attribute__((unused)))
 {
         int int_mode;
 
-        while (int_mode && (int_mode != EOF))
+        while (int_mode)
         {
                 int_mode = isatty(STDIN_FILENO);
                 if (int_mode == 1)
