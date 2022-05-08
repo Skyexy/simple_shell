4e2539330402e2426cc37f5560eae4e12c443a11
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-05-08 17:35:19 +0100

    Update simple_shell.c

--- simple_shell.c
+++ simple_shell.c
@@ -5,7 +5,8 @@ int main(void)
         int int_mode;
         size_t buff = 1024;
         int counter = 0;
-        char **toks = malloc(sizeof(char *) * 1024);
+        int status;
+        char *toks[buff];
         char *input = malloc(sizeof(char) * buff);
         char *token = malloc(sizeof(char) * buff);
         
@@ -14,7 +15,7 @@ int main(void)
                 int_mode = isatty(STDIN_FILENO);
                 if (int_mode == 1)
                 {
-                        write(STDOUT_FILENO, "#cisfun$" , 13);
+                        write(STDOUT_FILENO, "#cisfun$ " , 8);
                 }
                 getline(&input, &buff, stdin);
                 token = strtok(input, " ");
