613cff188b1289cf418b362572c2834f21183887
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-05-08 17:03:56 +0100

    Update main.h

--- main.h
+++ main.h
@@ -1,14 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-typedef struct ali {
-     char *name;
-     int (*func)(void);
-} alias;
-alias aliases[] = {
-     {"name", function},
-     {NULL, NULL}
-};
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
