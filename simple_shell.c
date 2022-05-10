03e6448955ba9142b5e9427b4a61d2bba5d2c7b2
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-05-11 00:42:45 +0100

    Update simple_shell.c

--- simple_shell.c
+++ simple_shell.c
@@ -1,50 +1,68 @@
 #include "main.h"
 
+#define BUFFER_LEN 1024
 
-int main(void)
-{
-        int int_mode;
-        size_t buff = 0;
-        char **argv = malloc(sizeof(char *) * 512);
-        char *input = malloc(sizeof(char) * 512);
-        char *token = NULL;
-        int counter;
-        pid_t son;
-        char *current = malloc(sizeof(char) * 512);
+int main(){
+    char *user_input;
+    char* argv[120];
+    int argc ;
+    char* path = "";
+    char file_path[50];
+    size_t buff;
+    char c;
 
-        while (int_mode != EOF)
+    while(c != EOF){
+        
+        write(STDOUT_FILENO, "($) ", 4);     
+        
+        if ((c = getline(&user_input,&buff, stdin)) == EOF)
         {
-                int_mode = isatty(STDIN_FILENO);
-                if (int_mode == 1)
-                {
-                        write(STDOUT_FILENO, "($) ", 4);
-                }
-                getline(&input,&buff,stdin);
-                if (*input == '\n')
-                {
-                        continue;
-                }
-                token = strtok(input, " ");
-                counter = 0;
-                while (token != NULL)
-                {
-                        argv[counter] = _strdunp(token);
-                        token = strtok(NULL, " ");
-                        counter++;
-                }
-                argv[counter] = NULL;
-                son = fork();
-                if (son == -1)
-                {
-                        perror("Error:");
-                        return (1);
-                }
-                if(!son)
+                break;
+        }
+        
+        size_t length = strlen(user_input);
+
+        if(length == 0){
+            break;
+        }
+
+        if (user_input[length - 1] == '\n'){
+            user_input[length - 1] = '\0'; // replace last char by '\0' if it is new line char
+        }
+        if (strcmp("exit",user_input) == 0)
+        {
+                break;
+        }
+        
+        //split command using spaces
+        char *token;                  
+        token = strtok(user_input," ");
+        int argc=0;
+        if(token == NULL){
+            continue;
+        }
+        while(token!=NULL){
+            argv[argc]=token;      
+            token = strtok(NULL," ");
+            argc++;
+        }
+        
+        argv[argc]=NULL; 
+        
+        strcpy(file_path, path);  //Assign path to file_path 
+        strcat(file_path, argv[0]); //conctanate command and file path           
+
+        if (access(file_path,F_OK)==0){  //check the command is available in /bin
+        
+            pid_t pid, wpid;
+            int status;
+            
+            pid = fork();
+            if(pid == 0)
                 {
                         if (execve(argv[0], argv, NULL) == -1)
                         {
-                                getcwd(current, 1204);
-                                perror(current);
+                                perror("Error");
                         }
                 }
                 else
@@ -52,9 +70,9 @@ int main(void)
                         wait(NULL);
                 }
         }
-        free(input);
-        free(token);
-        free(argv);
-        free(current);
-        return (0);
-}
+        else {
+            printf("Command is not available in the bin\n"); //Command is not available in the bin
+        }
+
+    }
+} 
