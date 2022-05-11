5c40fdb8436a35b25270fe2d0a424fdec0acfa3d
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-05-11 02:32:26 +0100

    Update main.h

--- main.h
+++ main.h
@@ -6,6 +6,24 @@
 #include <string.h>
 #include <sys/wait.h>
 
+typedef struct ali {
+     char *name;
+     int (*func)(void);
+} alias;
+alias aliases[] = {
+     {"name", function},
+     {NULL, NULL}
+};
+typedef struct bin{
+     char *name;
+     int (*func)(void);
+} builtin;
+builtin builtins[] = {
+     {"exit", exit_function},
+     {"env", env_function},
+     {NULL, NULL}
+};
+
 int _strlen(char *s);
 char *_strncat(char *dest, char *src, int n);
 char *_strdunp(char *lo);
