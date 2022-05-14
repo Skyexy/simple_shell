bbf098c70dbcda77454362ab76df0ff3f9bd50e2
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-05-14 22:38:22 +0100

    Update simple_shell.c

--- simple_shell.c
+++ simple_shell.c
@@ -3,17 +3,17 @@
 #define BUFFER_LEN 1024
 
 int main(){
-    char *user_input;
-    char* argv[120];
-    int argc ;
-    char* path = "";
-    char file_path[50];
+    char *user_input = malloc(sizeof(char) * BUFFER_LEN);
+    char *argv[BUFFER_LEN];
+    int argc;
     size_t buff;
     char c;
+    int x;
+    char *token = NULL;
 
     while(c != EOF){
         
-        write(STDOUT_FILENO, "($) ", 4);     
+        write(STDOUT_FILENO, "($) ", 4);
         
         if ((c = getline(&user_input,&buff, stdin)) == EOF)
         {
@@ -27,52 +27,15 @@ int main(){
         }
 
         if (user_input[length - 1] == '\n'){
-            user_input[length - 1] = '\0'; // replace last char by '\0' if it is new line char
+            user_input[length - 1] = '\0';
         }
-        if (strcmp("exit",user_input) == 0)
+        x = check(user_input);
+        if (x > 0)
         {
-                break;
+                comma(user_input);
         }
         
-        //split command using spaces
-        char *token;                  
-        token = strtok(user_input," ");
-        int argc=0;
-        if(token == NULL){
-            continue;
-        }
-        while(token!=NULL){
-            argv[argc]=token;      
-            token = strtok(NULL," ");
-            argc++;
-        }
-        
-        argv[argc]=NULL; 
-        
-        strcpy(file_path, path);  //Assign path to file_path 
-        strcat(file_path, argv[0]); //conctanate command and file path           
-
-        if (access(file_path,F_OK)==0){  //check the command is available in /bin
-        
-            pid_t pid, wpid;
-            int status;
-            
-            pid = fork();
-            if(pid == 0)
-                {
-                        if (execve(argv[0], argv, NULL) == -1)
-                        {
-                                perror("Error");
-                        }
-                }
-                else
-                {
-                        wait(NULL);
-                }
-        }
-        else {
-            printf("Command is not available in the bin\n"); //Command is not available in the bin
-        }
-
+        divd(user_input);
     }
+    return (0);
 } 
