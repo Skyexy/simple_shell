6c5164258408cfeafa70534466ff8301d157204c
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-05-18 12:33:14 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,37 +1,36 @@
 #include "main.h"
 
 int main(){
-    char *user_input = malloc(sizeof(char) * BUFFER_LEN);
-    char *argv[BUFFER_LEN];
-    int argc;
-    size_t buff;
-    char c;
-    int x;
-    char *token = NULL;
+	char *user_input = malloc(sizeof(char) * BUFFER_LEN);
+	size_t buff;
+	char c;
+	int x;
+	size_t length;
 
-    while(c != EOF){
+	while(c != EOF)
+	{
+		write(STDOUT_FILENO, "($) ", 4);
         
-        write(STDOUT_FILENO, "($) ", 4);
+		if ((c = getline(&user_input,&buff, stdin)) == EOF)
+		{
+			break;
+		}
         
-        if ((c = getline(&user_input,&buff, stdin)) == EOF)
-        {
-                break;
-        }
-        
-        size_t length = strlen(user_input);
+		size_t length = strlen(user_input);
 
-        if(length == 0){
-            break;
-        }
-
-        if (user_input[length - 1] == '\n'){
-            user_input[length - 1] = '\0';
-        }
-        if (x = seprators(user_input) == 1)
-        {
-                continue;
-        }
-        divd(user_input);
-    }
-    return (0);
-} 
+		if(length == 0)
+		{
+			break;
+		}
+		if (user_input[length - 1] == '\n')
+		{
+			user_input[length - 1] = '\0';
+		}
+		if (x = seprators(user_input) == 1)
+		{
+			continue;
+		}
+		divd(user_input);
+	}
+	return (0);
+}
